514745035c47d952ee733201a245e532d34d499e
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-09-15 13:02:09 -0300

    Added QuotationService

---
+++ cotacao/src/main/java/br/com/mineradora/client/CurrencyPriceClient.java
@@ -0,0 +1,20 @@
+package br.com.mineradora.client;
+
+import br.com.mineradora.dto.CurrencyPriceDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+
+@Path("/last")
+@RegisterRestClient(baseUri="https://economia.awesomeapi.com.br")
+@ApplicationScoped
+public interface CurrencyPriceClient {
+
+    @GET
+    @Path("/{pair}")
+    CurrencyPriceDTO getPriceByPair(@PathParam("pair") String pair);
+
+}
---
+++ cotacao/src/main/java/br/com/mineradora/scheduler/QuotationScheduler.java
@@ -0,0 +1,25 @@
+package br.com.mineradora.scheduler;
+
+import br.com.mineradora.service.QuotationService;
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class QuotationScheduler {
+
+    private final Logger LOG = LoggerFactory.getLogger(QuotationScheduler.class);
+
+    @Inject
+    QuotationService quotationService;
+
+    @Transactional
+    @Scheduled(every = "35s", identity="task-job")
+    void schedule(){
+        LOG.info("-- Executando scheduler --");
+        quotationService.getCurrencyPrice();
+    }
+}
